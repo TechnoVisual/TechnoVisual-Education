23a82a43c3bc4d139804b51ecb9f5009294a8adc
Mark Vanstone <mark@technovisual.co.uk>
2019-03-29 11:43:39 +0000

    First commit
    Source for MagPi article

---
+++ Party Popper/figure2.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+pwm=GPIO.PWM(3,50)
+pwm.start(0)
+
+def setAngle(angle):
+    duty=angle/18+2
+    GPIO.output(3, True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(3, False)
+    pwm.ChangeDutyCycle(0)
+
+setAngle(0)
+
+pwm.stop()
+GPIO.cleanup()
